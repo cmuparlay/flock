b537322538791d7568d31b55e217a0e50298ca52
Guy Blelloch <guyb@cs.cmu.edu>
2022-11-16 17:34:44 -0500

    added verlib.h

---
+++ include/flock/verlib.h
@@ -0,0 +1,45 @@
+// This selects between using persistent objects or regular objects
+// Persistent objects are implemented as described in:
+//   Wei, Ben-David, Blelloch, Fatourou, Rupert and Sun,
+//   Constant-Time Snapshots with Applications to Concurrent Data Structures
+//   PPoPP 2021
+// They support snapshotting via version chains, and without
+// indirection, but pointers to objects (ptr_type) must be "recorded
+// once" as described in the paper.
+#pragma once
+
+#include "flock.h"
+
+#ifdef Persistent
+
+// persistent objects, ptr_type includes version chains
+#ifdef Recorded_Once
+#include "versioned_recorded_once.h"
+#elif Simple_Recorded_Once_
+#include "simple_persistent_recorded_once.h"
+#elif FullyIndirect
+#include "versioned_indirect.h"
+#elif Simple
+#include "versioned_simple.h"
+#else
+#include "versioned.h"
+#endif
+
+# else // Not Persistent
+
+namespace vl {
+
+struct versioned {};
+
+template <typename T>
+using versioned_ptr = flck::atomic<T*>;
+}
+
+#endif
+
+namespace vl {
+  using flck::with_epoch;
+  using flck::memory_pool;
+}
+
+
