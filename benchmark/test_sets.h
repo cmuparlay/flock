5a5a2cde69f2594c69dc57e529cbb5003530d286
Hao-Wei <hao.wei5838@gmail.com>
2022-12-29 09:24:15 -0500

    minor changes and fixed indentation

--- benchmark/test_sets.h
+++ benchmark/test_sets.h
@@ -54,7 +54,7 @@ void test_persistence_concurrent(SetType& os) {
       while(!done) {
         bool seen[N+1];
         int max_seen;
-	      vl::with_snapshot([&] {
+        vl::with_snapshot([&] {
           max_seen = -1;
           for(int i = 1; i <= N; i++) seen[i] = false;
           for(int i = 1; i <= N; i++) {
@@ -218,16 +218,17 @@ void test_sets(SetType& os, size_t default_size, commandLine P) {
       //max_key = ~0ul >> 1;
       max_key = ~0ul;
       auto x = parlay::delayed_tabulate(1.2*nn,[&] (size_t i) {
-			 return (key_type) parlay::hash64(i);}); // generate 64-bit keys
+       return (key_type) parlay::hash64(i);}); // generate 64-bit keys
       //return (key_type) ((parlay::hash64(i) << 1) >> 1);}); // generate 63-bit keys
       auto xx = parlay::remove_duplicates(x);
       auto y = parlay::random_shuffle(xx);
       // don't use zero since it breaks setbench code
-      a = parlay::tabulate(nn, [&] (size_t i) {return std::min(max_key,y[i]+1);});
+      // following lines seem problematic, y[i]+1 has the potential to overflow and pick 0, not sure why max_key is there, seems like it doesn't change anything.
+      a = parlay::tabulate(nn, [&] (size_t i) {return std::min(max_key,y[i]+1);}); 
     } else {
       max_key = nn;
       a = parlay::random_shuffle(parlay::tabulate(nn, [] (key_type i) {
-					   return i+1;}));
+             return i+1;}));
     }
     key_type range_gap = (max_key/n)*range_size;
 
@@ -245,12 +246,12 @@ void test_sets(SetType& os, size_t default_size, commandLine P) {
 
     op_types = parlay::tabulate(m, [&] (size_t i) -> op_type {
         auto h = parlay::hash64(m+i)%200;
-	if (h < update_percent) return Insert; 
-	else if (h < 2*update_percent) return Remove;
-	else if (h < 2*update_percent + 2*range_percent) return Range;
-	else if (h < 2*update_percent + 2*range_percent + 2*multifind_percent)
-	  return MultiFind;
-	else return Find; });
+        if (h < update_percent) return Insert; 
+        else if (h < 2*update_percent) return Remove;
+        else if (h < 2*update_percent + 2*range_percent) return Range;
+        else if (h < 2*update_percent + 2*range_percent + 2*multifind_percent)
+          return MultiFind;
+        else return Find; });
     
     parlay::internal::timer t;
     if (shuffle) os.shuffle(n);
@@ -259,227 +260,228 @@ void test_sets(SetType& os, size_t default_size, commandLine P) {
       auto tr = os.empty(buckets);
       long len;
       if (do_check) {
-	size_t len = os.check(tr);
-	if (len != 0) {
-	  std::cout << "BAD LENGTH = " << len << std::endl;
-	} else if(verbose) {
-	  std::cout << "CHECK PASSED" << std::endl;
-	}
+        size_t len = os.check(tr);
+        if (len != 0) {
+          std::cout << "BAD LENGTH = " << len << std::endl;
+        } else if(verbose) {
+          std::cout << "CHECK PASSED" << std::endl;
+        }
       }
       
       if (verbose) std::cout << "round " << i << std::endl;
       if (fixed_time) {
         size_t initial_size = n;
-	if (balanced_tree) {
-	  auto x = parlay::sort(parlay::remove_duplicates(a.head(n)));
-	  auto y = x.head(x.size());
-	  insert_balanced(os, tr, y);
-	} else {
+        if (balanced_tree) {
+          auto x = parlay::sort(parlay::remove_duplicates(a.head(n)));
+          auto y = x.head(x.size());
+          insert_balanced(os, tr, y);
+        } else {
 
-	  parlay::parallel_for(0, n, [&] (size_t i) {
-		 os.insert(tr, a[i], 123); }, 10, true);
+          parlay::parallel_for(0, n, [&] (size_t i) {
+           os.insert(tr, a[i], 123); }, 10, true);
 
-	  parlay::parallel_for(0, 2*nn, [&] (size_t i) {
-		 auto k = a[parlay::hash64(m + i) % nn];
-		 if(i%2==0) os.insert(tr, k, 123); 
-		 else os.remove(tr, k); }, 10, true);
+          parlay::parallel_for(0, 2*nn, [&] (size_t i) {
+           auto k = a[parlay::hash64(2*m + i) % nn];
+           if(i%2==0) os.insert(tr, k, 123); 
+           else os.remove(tr, k); }, 10, true);
 
-	  if (do_check) {
-	    size_t expected = n;
-	    size_t got = os.check(tr);
-	    initial_size = got;
-	    double ratio = 1.0*got/expected;
-	    if (ratio < 0.9 || ratio > 1.1) {
-	      std::cout << "warning: expected approxiamtely " << expected
-			<< " keys after random inserts and deletes, found " << got << std::endl;
-	    } else if(verbose) {
-	      std::cout << "CHECK PASSED" << std::endl;
-	    }
-	  }
-	}
+          if (do_check) {
+            size_t expected = n;
+            size_t got = os.check(tr);
+            initial_size = got;
+            double ratio = 1.0*got/expected;
+            if (ratio < 0.9 || ratio > 1.1) {
+              std::cout << "warning: expected approxiamtely " << expected
+            << " keys after random inserts and deletes, found " << got << std::endl;
+            } else if(verbose) {
+              std::cout << "CHECK PASSED" << std::endl;
+            }
+          }
+        }
 
         parlay::sequence<size_t> totals(p);
-	parlay::sequence<long> addeds(p);
-	parlay::sequence<long> range_counts(p);
-	parlay::sequence<long> mfind_counts(p);
-	parlay::sequence<long> retry_counts(p);
-	parlay::sequence<long> update_counts(p);
-	parlay::sequence<long> query_counts(p);
+        parlay::sequence<long> addeds(p);
+        parlay::sequence<long> range_counts(p);
+        parlay::sequence<long> mfind_counts(p);
+        parlay::sequence<long> retry_counts(p);
+        parlay::sequence<long> update_counts(p);
+        parlay::sequence<long> query_counts(p);
         size_t mp = m/p;
         t.start();
         auto start = std::chrono::system_clock::now();
         std::atomic<bool> finish = false;
         parlay::parallel_for(0, p, [&] (size_t i) {
-             int cnt = 0;
-             size_t j = i*mp;
-             size_t total = 0;
-	     long added = 0;
-	     long range_count = 0;
-	     long mfind_count = 0;
-	     long retry_count = 0;
-	     long update_count = 0;
-	     long query_count = 0;
-	     volatile long keysum = 0;
-             while (true) {
-               // every once in a while check if time is over
-               if (cnt >= 100) { 
-                 cnt = 0;
-		 auto current = std::chrono::system_clock::now();
-		 double duration = std::chrono::duration_cast<std::chrono::milliseconds>(current - start).count();
-                 if (duration > 1000*trial_time || finish) {
-                   totals[i] = total;
-		   addeds[i] = added;
-		   range_counts[i] = range_count;
-		   mfind_counts[i] = mfind_count;
-		   retry_counts[i] = retry_count;
-		   update_counts[i] = update_count;
-		   query_counts[i] = query_count;
-                   return;
-                 }
-               }
-               // check that no overflow
-               if (j >= (i+1)*mp) abort();
-	       
-	       if (op_types[j] == Find) {
-		 query_count++;
-		 auto val = os.find(tr, b[j]);
-		 if(val.has_value()) keysum += val.value();
-	       }
-	       else if (op_types[j] == Insert) {
-		 update_count++;
-		 if (os.insert(tr, b[j], 123)) added++;}
-	       else if (op_types[j] == Remove) {
-		 update_count++;
-		 if (os.remove(tr, b[j])) added--;}
-	       else if (op_types[j] == Range) {
+          int cnt = 0;
+          size_t j = i*mp;
+          size_t total = 0;
+          long added = 0;
+          long range_count = 0;
+          long mfind_count = 0;
+          long retry_count = 0;
+          long update_count = 0;
+          long query_count = 0;
+          volatile long keysum = 0;
+          while (true) {
+            // every once in a while check if time is over
+            if (cnt >= 100) { 
+              cnt = 0;
+              auto current = std::chrono::system_clock::now();
+              double duration = std::chrono::duration_cast<std::chrono::milliseconds>(current - start).count();
+              if (duration > 1000*trial_time || finish) {
+                totals[i] = total;
+                addeds[i] = added;
+                range_counts[i] = range_count;
+                mfind_counts[i] = mfind_count;
+                retry_counts[i] = retry_count;
+                update_counts[i] = update_count;
+                query_counts[i] = query_count;
+                return;
+              }
+            }
+            // check that no overflow
+            if (j >= (i+1)*mp) abort();
+         
+            if (op_types[j] == Find) {
+              query_count++;
+              auto val = os.find(tr, b[j]);
+              if(val.has_value()) keysum += val.value();
+            }
+            else if (op_types[j] == Insert) {
+              update_count++;
+              if (os.insert(tr, b[j], 123)) added++;}
+            else if (op_types[j] == Remove) {
+              update_count++;
+              if (os.remove(tr, b[j])) added--;}
+            else if (op_types[j] == Range) {
 #ifdef Range_Search
-		 key_type end = ((b[j] > max_key - range_gap)
-				 ? max_key : b[j] + range_gap);
-		 range_count += vl::with_snapshot([&] {
-				    long cnt=0;
-				    auto addf = [&] (K x, V y) { cnt++;};
-				    os.range_(tr, addf, b[j], end);
+              key_type end = ((b[j] > max_key - range_gap)
+                             ? max_key : b[j] + range_gap);
+              range_count += vl::with_snapshot([&] {
+                long cnt=0;
+                auto addf = [&] (K x, V y) { cnt++;};
+                os.range_(tr, addf, b[j], end);
 #ifdef LazyStamp
-				    if (vl::aborted) retry_count++;
+                if (vl::aborted) retry_count++;
 #endif
-				    return cnt;});
+                return cnt;});
 #endif
-	       } else { // multifind
-		 mfind_count++;
-		 keysum += vl::with_snapshot([&] {
-	           long tmp_sum = 0;
-		   long loc = j;
-		   for (long k = 0; k < range_size; k++) {
-		     auto val = os.find_(tr, b[loc]);
-		     loc += 1;
-		     if (loc >= (i+1)*mp) loc -= mp;
-		     if(val.has_value()) tmp_sum += val.value();
+            } else { // multifind
+              mfind_count++;
+              keysum += vl::with_snapshot([&] {
+                long tmp_sum = 0;
+                long loc = j;
+                for (long k = 0; k < range_size; k++) {
+                  auto val = os.find_(tr, b[loc]);
+                  loc += 1;
+                  if (loc >= (i+1)*mp) loc -= mp;
+                  if(val.has_value()) tmp_sum += val.value();
 #ifdef LazyStamp
-		     if (vl::aborted) {
-		       retry_count++;
-		       return 0l;
-		     }
+                  if (vl::aborted) {
+                    retry_count++;
+                    return 0l;
+                  }
 #endif
-		   }
-		   return tmp_sum;});
-		 j += range_size;
-		 if (j >= (i+1)*mp) j -= mp;
-		 cnt += range_size;
-		 total += range_size;
-		 continue;
-	       }
-	       if (++j >= (i+1)*mp) j -= mp;
-               cnt++;
-               total++;
-             }}, 1);
+                }
+                return tmp_sum;});
+              j += range_size;
+              if (j >= (i+1)*mp) j -= mp;
+              cnt += range_size;
+              total += range_size;
+              continue;
+            }
+            if (++j >= (i+1)*mp) j -= mp;
+            cnt++;
+            total++;
+          }
+        }, 1);
         double duration = t.stop();
 
-	if (i != 0) { // don't report zeroth round -- warmup
-	  if (finish && (duration < trial_time/4))
-	    std::cout << "warning out of samples, finished in "
-		      << duration << " seconds" << std::endl;
+        if (i != 0) { // don't report zeroth round -- warmup
+          if (finish && (duration < trial_time/4))
+            std::cout << "warning out of samples, finished in "
+                << duration << " seconds" << std::endl;
 
-	  //std::cout << duration << " : " << trial_time << std::endl;
-	  size_t num_ops = parlay::reduce(totals);
-	  std::cout << std::setprecision(4)
-		    << P.commandName() << ","
-		    << update_percent << "%update,"
-		    << range_percent << "%range,"
-		    << multifind_percent << "%mfind,"
-		    << "rs=" << range_size << ","
-		    << "n=" << n << ","
-		    << "p=" << p << ","
-		    << "z=" << zipfian_param << ","
-		    << num_ops / (duration * 1e6) << std::endl;
-    // std::cout << "timestamp increments (per microsecond): " << (global_stamp.get_stamp()-start_timestamp)*1.0/(duration * 1e6) << std::endl;
-    // std::cout << "final timestamp: " << global_stamp.get_stamp() << std::endl;
-	  if (do_check) {
-	    size_t final_cnt = os.check(tr);
-	    long updates = parlay::reduce(addeds);
-	    if (multifind_percent > 0) {
-	      long mfind_sum = parlay::reduce(mfind_counts);
-	      long retry_sum = parlay::reduce(retry_counts);
-#ifdef LazyStamp
-	      std::cout << "retry percent = " << (double) 100 * retry_sum / mfind_sum
-			<< std::endl;
-#endif
-	    }
-	    if (range_percent > 0) {
-	      long range_sum = parlay::reduce(range_counts);
-	      long retry_sum = parlay::reduce(retry_counts);
-	      long num_queries = num_ops * range_percent / 100;
-	      std::cout << "average range size = " << ((float) range_sum) / num_queries
-#ifdef LazyStamp
-			<< ", retry percent = " << (double) 100 * retry_sum / num_queries 
-#endif
-			<< std::endl;
-	    }
+          //std::cout << duration << " : " << trial_time << std::endl;
+          size_t num_ops = parlay::reduce(totals);
+          std::cout << std::setprecision(4)
+              << P.commandName() << ","
+              << update_percent << "%update,"
+              << range_percent << "%range,"
+              << multifind_percent << "%mfind,"
+              << "rs=" << range_size << ","
+              << "n=" << n << ","
+              << "p=" << p << ","
+              << "z=" << zipfian_param << ","
+              << num_ops / (duration * 1e6) << std::endl;
+          // std::cout << "timestamp increments (per microsecond): " << (global_stamp.get_stamp()-start_timestamp)*1.0/(duration * 1e6) << std::endl;
+          // std::cout << "final timestamp: " << global_stamp.get_stamp() << std::endl;
+          if (do_check) {
+            size_t final_cnt = os.check(tr);
+            long updates = parlay::reduce(addeds);
+            if (multifind_percent > 0) {
+              long mfind_sum = parlay::reduce(mfind_counts);
+              long retry_sum = parlay::reduce(retry_counts);
+      #ifdef LazyStamp
+              std::cout << "retry percent = " << (double) 100 * retry_sum / mfind_sum
+            << std::endl;
+      #endif
+            }
+            if (range_percent > 0) {
+              long range_sum = parlay::reduce(range_counts);
+              long retry_sum = parlay::reduce(retry_counts);
+              long num_queries = num_ops * range_percent / 100;
+              std::cout << "average range size = " << ((float) range_sum) / num_queries
+      #ifdef LazyStamp
+            << ", retry percent = " << (double) 100 * retry_sum / num_queries 
+      #endif
+            << std::endl;
+            }
 
-	    if (initial_size + updates != final_cnt) {
-	      std::cout << "bad size: intial size = " << initial_size 
-			<< ", added " << updates
-			<< ", final size = " << final_cnt 
-			<< std::endl;
-	    } else if(verbose) {
-              std::cout << "CHECK PASSED" << std::endl;
+            if (initial_size + updates != final_cnt) {
+              std::cout << "bad size: intial size = " << initial_size 
+                        << ", added " << updates
+                        << ", final size = " << final_cnt 
+                        << std::endl;
+            } else if(verbose) {
+                    std::cout << "CHECK PASSED" << std::endl;
             }
           }
-	  // if (stats) {
-	  //   descriptor_pool.stats();
-	  //   log_array_pool.stats();
-	  //   os.stats();
-	  // }
-	}
+          // if (stats) {
+          //   descriptor_pool.stats();
+          //   log_array_pool.stats();
+          //   os.stats();
+          // }
+        }
         parlay::parallel_for(0, nn, [&] (size_t i) { os.remove(tr, a[i]); });
       }
       else {
         // millions of operations per second
         auto mops = [=] (double time) -> float {return m / (time * 1e6);};
-	long len = parlay::remove_duplicates(b).size();
-	parlay::sequence<bool> flags(m);
+        long len = parlay::remove_duplicates(b).size();
+        parlay::sequence<bool> flags(m);
 
-	
+  
         t.start();
         parlay::parallel_for(0, m, [&] (size_t i) {
                    flags[i] = os.insert(tr, b[i], 123);
-                 });
+        });
 
-	//os.print(tr);
+        //os.print(tr);
         std::cout << "insert," << m << "," << mops(t.stop()) << std::endl;
 
         if (do_check) {
-	  long total = parlay::count(flags, true);
-	  long lenc = os.check(tr);
-	  if (lenc != len || total != len) {
-	    std::cout << "incorrect size after insert: inserted="
-		      << len << " succeeded=" << total << " found=" << lenc
-		      << std::endl;
-	    //os.print(tr);
-	    auto x = parlay::sort(parlay::remove_duplicates(b));
-	    //for (auto y : x)
-	    //  std::cout << y << std::endl;
-	  }
-	}
+          long total = parlay::count(flags, true);
+          long lenc = os.check(tr);
+          if (lenc != len || total != len) {
+            std::cout << "incorrect size after insert: inserted="
+                << len << " succeeded=" << total << " found=" << lenc
+                << std::endl;
+            //os.print(tr);
+            auto x = parlay::sort(parlay::remove_duplicates(b));
+            //for (auto y : x)
+            //  std::cout << y << std::endl;
+          }
+        }
         if (stats) {
           //descriptor_pool.stats();
           //log_array_pool.stats();
@@ -516,9 +518,9 @@ void test_sets(SetType& os, size_t default_size, commandLine P) {
       }
       os.retire(tr); // free the ord_set (should be empty, but not with arttree)
       if (clear) {
-	os.clear();
-	//descriptor_pool.clear();
-	//log_array_pool.clear();
+        os.clear();
+        //descriptor_pool.clear();
+        //log_array_pool.clear();
       }
       if (stats) {
         if (clear) std::cout << "the following should be zero if no memory leak" << std::endl;
