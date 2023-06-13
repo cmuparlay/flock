1240b101402a6cd34fcba074cf506dd342873058
Guy Blelloch <guyb@cs.cmu.edu>
2023-06-12 23:32:40 -0400

    cleanup of flock_grow

--- test/structures/flock_grow/unordered_map.h
+++ test/structures/flock_grow/unordered_map.h
@@ -16,27 +16,34 @@ template <typename K,
 struct unordered_map {
 
 private:
-  static constexpr int exp_factor = 16;
+  static constexpr int log_exp_factor = 4;
+  static constexpr int exp_factor = 1 << log_exp_factor;
   static constexpr int block_size = 64;
   static constexpr int overflow_size = 8;
 
   struct KV {K key; V value;};
   
   template <typename Range>
-  static int find_key(const Range& entries, long cnt, const K& k) {
+  static int find_in_range(const Range& entries, long cnt, const K& k) {
     for (int i=0; i < cnt; i++)
       if (KeyEqual{}(entries[i].key, k)) return i;
     return -1;
   }
 
+  // The following three functions copy a range and
+  // insert/update/remove the specified key.  No ordering is assumed
+  // within the range.  Insert assumes k does not appear, while
+  // update/remove assume it does appear.
   template <typename Range, typename RangeIn>
-  static void insert(Range& out, const RangeIn& entries, long cnt, const K& k, const V& v) {
+  static void copy_and_insert(Range& out, const RangeIn& entries, long cnt,
+			      const K& k, const V& v) {
     for (int i=0; i < cnt; i++) out[i] = entries[i];
     out[cnt] = KV{k,v};
   }
 
   template <typename Range, typename RangeIn>
-  static void update(Range& out, const RangeIn& entries, long cnt, const K& k, const V& v) {
+  static void copy_and_update(Range& out, const RangeIn& entries, long cnt,
+			      const K& k, const V& v) {
     int i = 0;
     while (!KeyEqual{}(k, entries[i].key)) {
       assert(i < cnt);
@@ -53,7 +60,7 @@ private:
   }
 
   template <typename Range, typename RangeIn>
-  static void remove(Range& out, const RangeIn& entries, long cnt, const K& k) {
+  static void copy_and_remove(Range& out, const RangeIn& entries, long cnt, const K& k) {
     int i = 0;
     while (!KeyEqual{}(k, entries[i].key)) {
       assert(i < cnt);
@@ -66,7 +73,9 @@ private:
     }
   }
 
-  // what each slot in table points to
+  // Each bucket points to a Node of some Size, or to a BigNode (defined below)
+  // A node contains an array of up to Size entries (actual # of entries given by cnt)
+  // Sizes are 1, 3, 7, 31
   template <int Size>
   struct Node {
     using node = Node<0>;
@@ -74,118 +83,153 @@ private:
     KV entries[Size];
     KV& get_entry(int i) {
       if (cnt <= 31) return entries[i];
+      // if bigger than 31, then it is a BigNode
       else return ((BigNode*) this)->entries[i];
     }
-    
-    int find(const K& k) {
-      if (cnt <= 31) return find_key(entries, cnt, k);
-      else return find_key(((BigNode*) this)->entries, cnt, k);
+
+    // return index of key in entries, or -1 if not found
+    int find_index(const K& k) {
+      if (cnt <= 31) return find_in_range(entries, cnt, k);
+      else return find_in_range(((BigNode*) this)->entries, cnt, k);
     }
 
-    std::optional<V> find_value(const K& k) {
+    // return optional value found in entries given a key
+    std::optional<V> find(const K& k) {
       if (cnt <= 31) { // regular node
-	int i = find(k);
+	int i = find_index(k);
 	if (i == -1) return {};
 	else return entries[i].value;
       } else { // big node
-	int i = find_key(((BigNode*) this)->entries, cnt, k);
+	int i = find_in_range(((BigNode*) this)->entries, cnt, k);
 	if (i == -1) return {};
 	else return ((BigNode*) this)->entries[i].value;
       }
     }
 
-    Node() {} 
+    // copy insert
     Node(node* old, const K& k, const V& v) {
       cnt = (old == nullptr) ? 1 : old->cnt + 1;
-      insert(entries, old->entries, cnt-1, k, v);
+      copy_and_insert(entries, old->entries, cnt-1, k, v);
     }
 
+    // copy update
     Node(node* old, const K& k, const V& v, bool x) : cnt(old->cnt) {
       assert(old != nullptr);
-      update(entries, old->entries, cnt, k, v);
+      copy_and_update(entries, old->entries, cnt, k, v);
     }
 
+    // copy remove
     Node(node* old, const K& k) : cnt(old->cnt - 1) {
-      if (cnt == 31) remove(entries, ((BigNode*) old)->entries, cnt+1, k);
-      else remove(entries, old->entries, cnt+1, k);
+      // if cnt==31 then needs to be copied from a BigNode into this regular node
+      if (cnt == 31) copy_and_remove(entries, ((BigNode*) old)->entries, cnt+1, k);
+      else copy_and_remove(entries, old->entries, cnt+1, k);
     }
   };
   using node = Node<0>;
 
   // If a node overflows (cnt > 31), then it becomes a big node and its content
   // is stored indirectly in a parlay sequences.
+  // This should rarely, if ever, happen.
   struct BigNode {
     using entries_type = parlay::sequence<KV>;
     int cnt;
     entries_type entries;
 
+    // copy insert
     BigNode(node* old, const K& k, const V& v) : cnt(old->cnt + 1) {
       entries = entries_type(cnt);
-      if (old->cnt == 31) insert(entries, old->entries, old->cnt, k, v);
-      else insert(entries, ((BigNode*) old)->entries, old->cnt, k, v);
+      // if old->cnt == 31 then need to copy from regular node into this BigNode
+      if (old->cnt == 31) copy_and_insert(entries, old->entries, old->cnt, k, v);
+      else copy_and_insert(entries, ((BigNode*) old)->entries, old->cnt, k, v);
     }
 
+    // copy update
     BigNode(node* old, const K& k, const V& v, bool x) : cnt(old->cnt) {
       entries = entries_type(cnt);
-      update(entries, ((BigNode*) old)->entries, cnt, k, v);  }
+      copy_and_update(entries, ((BigNode*) old)->entries, cnt, k, v);  }
 
+    // copy remvoe
     BigNode(node* old, const K& k) : cnt(old->cnt - 1) {
       entries = entries_type(cnt);
-      remove(entries, ((BigNode*) old)->entries, cnt+1, k); }
+      copy_and_remove(entries, ((BigNode*) old)->entries, cnt+1, k); }
   };
 
-  using slot = std::atomic<node*>;
+  // one bucket of a sequence of buckets
+  using bucket = std::atomic<node*>;
+
+  // status of a block
   enum status : char {Empty, Working, Done};
-  
-  struct prim_table {
-    std::atomic<prim_table*> next;
-    std::atomic<long> count;
-    long bits;
-    size_t size;
-    parlay::sequence<slot> buckets;
-    parlay::sequence<std::atomic<status>> block_status;
-    slot* get_slot(const K& k) {
-      return &buckets[get_index(k)];
-    }
+
+  // a single version of the table
+  // this can change as the table grows
+  struct table_version {
+    std::atomic<table_version*> next; // points to next version if created
+    std::atomic<long> finished_block_count; //number of blocks finished copying
+    long bits;  // log_2 of size
+    size_t size; // number of buckets
+    parlay::sequence<bucket> buckets; // sequence of buckets
+    parlay::sequence<std::atomic<status>> block_status; // status of each block while copying
+
     long get_index(const K& k) {
-      return (Hash{}(k) >> (40 - bits))  & (size-1u);
-    }
-    prim_table(long n) : next(nullptr), count(0) {
-      bits = std::max<long>(6, 1 + parlay::log2_up(256)); 
-      size = 1ul << bits; // needs to be at least block_size
-      buckets = parlay::sequence<slot>(size);
-    }
-    prim_table(prim_table* t)
-      : next(nullptr), count(0), bits(t->bits + 4), size(t->size*exp_factor),
+      return (Hash{}(k) >> (40 - bits))  & (size-1u);}
+
+    bucket* get_bucket(const K& k) {
+      return &buckets[get_index(k)]; }
+
+    // initial table version, n indicating initial size
+    table_version(long n)
+      : next(nullptr),
+	finished_block_count(0),
+	bits(1 + parlay::log2_up(std::max<long>(block_size, n))),
+	size(1ul << bits), 
+	buckets(parlay::sequence<bucket>(size)) {}
+
+    // expanded table versions copied from smaller version t
+    table_version(table_version* t)
+      : next(nullptr),
+	finished_block_count(0),
+	bits(t->bits + log_exp_factor),
+	size(t->size * exp_factor),
 	buckets(parlay::sequence<std::atomic<node*>>::uninitialized(size)),
 	block_status(parlay::sequence<std::atomic<status>>(t->size/block_size)) {
       std::fill(block_status.begin(), block_status.end(), Empty);
     }
   };
-  
-  static flck::memory_pool<prim_table> prim_table_pool;
-  std::atomic<prim_table*> hash_table;
 
-  static node* tag_table(prim_table* x) {return (node*) (((size_t) x) | 1);}
-  static prim_table* untag_table(node* x) {return (prim_table*) (((size_t) x) & ~1ul);}
-  static bool is_tagged(node* x) {return ((size_t) x) & 1;}
+  // the current table version
+  // points to next larger table version if one exists
+  std::atomic<table_version*> current_table_version;
 
+  // memory pool for maintaining the table versions
+  // allocated dynamically although at most log(final_size) of them 
+  static flck::memory_pool<table_version> table_version_pool;
+
+  // A forwarded node indicates that entries should be accessed in the
+  // next larger table.  Uses a pointer of value 1.
+  static node* forwarded_node() {return (node*) 1;}
+  static bool is_forwarded(node* x) {return x == (node*) 1;}
+
+  // called when table should be expanded (i.e. when some bucket is too large)
+  // allocates a new table version and links the old one to it
   void expand_table() {
-    prim_table* ht = hash_table.load();
+    table_version* ht = current_table_version.load();
     if (ht->next == nullptr) {
       long n = ht->buckets.size();
-      prim_table* new_table = prim_table_pool.new_obj(ht);
-      prim_table* old = nullptr;
+      table_version* new_table = table_version_pool.new_obj(ht);
+      table_version* old = nullptr;
       if (!ht->next.compare_exchange_strong(old, new_table))
-	prim_table_pool.retire(new_table);
+	table_version_pool.retire(new_table);
       //else std::cout << "expand to: " << n * exp_factor << std::endl;
     }
   }
 
-  void copy(prim_table* t, KV& key_value) {
+  // copies key_value into a new table
+  // note this is not thread safe...i.e. only this thread should be
+  // updating the bucket corresponding to the key.
+  void copy(table_version* t, KV& key_value) {
     size_t idx = t->get_index(key_value.key);
     node* x = t->buckets[idx].load();
-    assert(!is_tagged(x));
+    assert(!is_forwarded(x));
     t->buckets[idx] = insert_to_node(x , key_value.key, key_value.value);
     destruct_node(x);
   }
@@ -194,8 +238,8 @@ private:
   // given by hashid is not already copied, tries to copy block_size buckets, including
   // that of hashid, to the next larger hash_table.
   void copy_if_needed(long hashid) {
-    prim_table* t = hash_table.load();
-    prim_table* next = t->next.load();
+    table_version* t = current_table_version.load();
+    table_version* next = t->next.load();
     if (next != nullptr) {
       long block_num = hashid & (next->block_status.size() -1);
       status st = next->block_status[block_num];
@@ -210,13 +254,15 @@ private:
 	  // Clear exp_factor buckets in the next table to put them in.
 	  for (int j = exp_start; j < exp_start + exp_factor; j++)
 	    next->buckets[j] = nullptr;
+	  // copy bucket to exp_factor new buckets in next table
 	  while (true) {
 	    node* bucket = t->buckets[i].load();
-	    assert(!is_tagged(bucket));
+	    assert(!is_forwarded(bucket));
 	    int cnt = (bucket == nullptr) ? 0 : bucket->cnt;
+	    // copy each element
 	    for (int j=0; j < cnt; j++) 
 	      copy(next, bucket->get_entry(j));
-	    bool succeeded = t->buckets[i].compare_exchange_strong(bucket,tag_table(next));
+	    bool succeeded = t->buckets[i].compare_exchange_strong(bucket,forwarded_node());
 	    if (succeeded) {
 	      retire_node(bucket);
 	      break;
@@ -235,9 +281,9 @@ private:
 	next->block_status[block_num] = Done;
 	// if all blocks have been copied then can set hash_table to next
 	// and retire the old table
-	if (++next->count == next->block_status.size()) {
-	  hash_table = next;
-	  prim_table_pool.retire(t);
+	if (++next->finished_block_count == next->block_status.size()) {
+	  current_table_version = next;
+	  table_version_pool.retire(t);
 	}
       } else {
 	// If working then wait until Done
@@ -302,26 +348,26 @@ private:
     else big_node_pool.destruct((BigNode*) old);
   }
 
-  std::optional<V> find_at(prim_table* t, slot* s, const K& k) {
+  std::optional<V> find_at(table_version* t, bucket* s, const K& k) {
     node* x = s->load();
-    if (is_tagged(x)) {
-      prim_table* nxt = t->next.load();
-      find_at(nxt, nxt->get_slot(k), k);
+    if (is_forwarded(x)) {
+      table_version* nxt = t->next.load();
+      find_at(nxt, nxt->get_bucket(k), k);
     }
     if (x == nullptr) return std::optional<V>();
     //KV& kv = x->get_entry(0);
     //if (KeyEqual{}(kv.key, k)) return kv.value;
-    return x->find_value(k);
+    return x->find(k);
   }
 		   
-  std::optional<bool> try_insert_at(prim_table* t, slot* s, const K& k, const V& v, bool upsert) {
+  std::optional<bool> try_insert_at(table_version* t, bucket* s, const K& k, const V& v, bool upsert) {
     node* x = s->load();
-    if (is_tagged(x)) {
-      prim_table* nxt = t->next.load();
-      return try_insert_at(nxt, nxt->get_slot(k), k, v, upsert);
+    if (is_forwarded(x)) {
+      table_version* nxt = t->next.load();
+      return try_insert_at(nxt, nxt->get_bucket(k), k, v, upsert);
     }
     node* new_node;
-    bool found = x != nullptr && x->find(k) != -1;
+    bool found = x != nullptr && x->find_index(k) != -1;
     if (found)
       if (upsert) new_node = update_node(x, k, v);
       else return false;
@@ -334,16 +380,16 @@ private:
     return {}; // failed
   }
   
-  static std::optional<bool> try_remove_at(prim_table* t, slot* s, const K& k) {
+  static std::optional<bool> try_remove_at(table_version* t, bucket* s, const K& k) {
     node* x = s->load();
-    if (is_tagged(x)) {
-      prim_table* nxt = t->next;
-      return try_remove_at(nxt, nxt->get_slot(k), k);
+    if (is_forwarded(x)) {
+      table_version* nxt = t->next;
+      return try_remove_at(nxt, nxt->get_bucket(k), k);
     }
-    if (x == nullptr || x->find(k) == -1)
+    if (x == nullptr || x->find_index(k) == -1)
       return false;
 
-    assert(x != nullptr && x->find(k) != -1);
+    assert(x != nullptr && x->find_index(k) != -1);
     node* new_node = remove_from_node(x, k);
     if(s->compare_exchange_strong(x, new_node)) {
       retire_node(x);
@@ -355,26 +401,26 @@ private:
 
 public:
 
-  unordered_map(size_t n) : hash_table(prim_table_pool.new_obj(n)) {}
+  unordered_map(size_t n) : current_table_version(table_version_pool.new_obj(n)) {}
 
   ~unordered_map() {
-    auto& buckets = hash_table.load()->buckets;
+    auto& buckets = current_table_version.load()->buckets;
     parlay::parallel_for (0, buckets.size(), [&] (size_t i) {
       retire_node(buckets[i].load());});
-    prim_table_pool.retire(hash_table.load());
+    table_version_pool.retire(current_table_version.load());
   }
 
   std::optional<V> find(const K& k) {
-    prim_table* ht = hash_table.load();
-    slot* s = ht->get_slot(k);
+    table_version* ht = current_table_version.load();
+    bucket* s = ht->get_bucket(k);
     __builtin_prefetch (s);
     return flck::with_epoch([=] {return find_at(ht, s, k);});
   }
 
   bool insert(const K& k, const V& v) {
-    prim_table* ht = hash_table.load();
+    table_version* ht = current_table_version.load();
     long idx = ht->get_index(k);
-    slot* s = &ht->buckets[idx];
+    bucket* s = &ht->buckets[idx];
     __builtin_prefetch (s);
     return flck::with_epoch([=] {
       return flck::try_loop([=] {
@@ -383,9 +429,9 @@ public:
   }
 
   bool upsert(const K& k, const V& v) {
-    prim_table* ht = hash_table.load();
+    table_version* ht = current_table_version.load();
     long idx = ht->get_index(k);
-    slot* s = &ht->buckets[idx];
+    bucket* s = &ht->buckets[idx];
     __builtin_prefetch (s);
     return flck::with_epoch([=] {
       return flck::try_loop([=] {
@@ -394,8 +440,8 @@ public:
   }
 
   bool remove(const K& k) {
-    prim_table* ht = hash_table.load();
-    slot* s = ht->get_slot(k);
+    table_version* ht = current_table_version.load();
+    bucket* s = ht->get_bucket(k);
     __builtin_prefetch (s);
     return flck::with_epoch([=] {
       return flck::try_loop([=] {
@@ -403,7 +449,7 @@ public:
   }
 
   long size() {
-    prim_table* ht = hash_table.load();
+    table_version* ht = current_table_version.load();
     if (ht->next != nullptr)
       for (int i=0; i < ht->size; i++)
 	copy_if_needed(i);
@@ -427,4 +473,4 @@ flck::memory_pool<typename unordered_map<K,V,H,E>::Node31> unordered_map<K,V,H,E
 template <typename K, typename V, typename H, typename E>
 flck::memory_pool<typename unordered_map<K,V,H,E>::BigNode> unordered_map<K,V,H,E>::big_node_pool;
 template <typename K, typename V, typename H, typename E>
-flck::memory_pool<typename unordered_map<K,V,H,E>::prim_table> unordered_map<K,V,H,E>::prim_table_pool;
+flck::memory_pool<typename unordered_map<K,V,H,E>::table_version> unordered_map<K,V,H,E>::table_version_pool;
